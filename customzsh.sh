40e5ae586a8c1bb0b9afe806caad43e44d7dd9ec
jpg <163206473+jpgjpgjpgjpg@users.noreply.github.com>
2025-02-03 07:19:31 -0300

    Create customzsh.sh

---
+++ customzsh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt install zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+mv kali/.zshrc ~/.zshrc
+
+source ~/.zshrc
