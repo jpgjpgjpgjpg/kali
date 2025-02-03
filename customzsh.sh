12a4ada8db6d7aa412ba5648041a89067ee51226
jpg <163206473+jpgjpgjpgjpg@users.noreply.github.com>
2025-02-03 08:02:28 -0300

    Update customzsh.sh

--- customzsh.sh
+++ customzsh.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sudo apt install zsh -y
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --skip-chsh
-
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
